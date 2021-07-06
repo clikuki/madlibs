31927d01418d28780ea3c12f6890af98a1d581b5
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-07-06 22:14:45 +0800

    slightly better

--- index.py
+++ index.py
@@ -1,54 +1,64 @@
 import madlibs
 
-def printMadlib(index):
+def printMadlib(madlibIndex):
     madlibs_string = ''
-    tuple_length = len(madlibs.madlibsTuple[index][0])
+    tuple_length = len(madlibs.templates[madlibIndex]['paragraph'])
     
-    i = 0
-    while i < tuple_length:
-        madlibs_string += madlibs.madlibsTuple[index][0][i]
+    for sentenceIndex in range(tuple_length):
+        madlibs_string += madlibs.templates[madlibIndex]['paragraph'][sentenceIndex]
         
-        if i != tuple_length - 1:
-            madlibs_string += handleInput(index, i, madlibs.madlibsTuple[index][1][i])
-
-        i += 1
+        if sentenceIndex != tuple_length - 1:
+            fill_in_str = handleInput(madlibIndex, sentenceIndex)
+            
+            if madlibs_string[len(madlibs_string) - 2:] == ' a':
+                if isVowel(fill_in_str[0]):
+                    madlibs_string += 'n'
+                madlibs_string += ' '
+            
+            madlibs_string += fill_in_str
         
     print(f'\n{madlibs_string}')
     
-def handleInput(madlib_index, input_index, type):
-    input_type = madlibs.madlibsTuple[madlib_index][1][input_index]
+def handleInput(madlib_index, input_index):
+    input_type = madlibs.templates[madlib_index]['fill_in'][input_index]
     suffix = getSuffix(input_type)
     
     if suffix != None:
-        fail_prefix = f'invalid {input_type[5:]} : '
+        inv_message_prefix = f'invalid {input_type[5:]} : '
         message = f'Enter a/an {input_type[5:]} that ends in -{suffix} : '
     else:
-        fail_prefix = f'invalid {input_type} : '
+        inv_message_prefix = f'invalid {input_type} : '
         message = f'Enter a/an {input_type}: '
     
     while True:
         user_input = input(message)
 
         if user_input == '':
-            print(f'{fail_prefix}must not be empty\n')
+            print(f'{inv_message_prefix}must not be empty\n')
             continue
             
-        if type == 'number':
-            if not validNum(user_input):
-                print(f'{fail_prefix}must be a number\n')
+        if input_type == 'number':
+            if not isValidNum(user_input):
+                print(f'{inv_message_prefix}must be a number\n')
                 continue
         else:
-            if validNum(user_input):
-                print(f'{fail_prefix}must not be a number\n')
+            if isValidNum(user_input):
+                print(f'{inv_message_prefix}must not be a number\n')
                 continue
-            elif suffix != None and user_input[-3:] != suffix:
-                print(f'{fail_prefix}must end in -{suffix}\n')
-                continue
+            elif suffix != None:
+                if user_input[-3:] != suffix:
+                    print(f'{inv_message_prefix}must end in -{suffix}\n')
+                    continue
+                
+                if user_input == suffix:
+                    print(f'{inv_message_prefix}must not be only -{suffix}\n')
+                    continue
         
         return user_input
-    
+
 def getIndex():
-    num_of_madlibs = len(madlibs.madlibsTuple)
+    num_of_madlibs = len(madlibs.templates)
+    inv_message_prefix = 'invalid index : '
     selection = ''
     
     for i in range(num_of_madlibs):
@@ -58,26 +68,26 @@ def getIndex():
             selection += ', '
     
     message = f'Enter {selection} to choose a Mad Lib: '
-        
+      
     while True:
         index = input(message)
         
         if index == '':
-            print('invalid index : must not be empty\n')
+            print(f'{inv_message_prefix}must not be empty\n')
             continue
 
-        if not validNum(index):
-            print(f'invalid index : must be a number\n')
+        if not isValidNum(index):
+            print(f'{inv_message_prefix}must be a number\n')
             continue
         
         if not 0 < int(index) <= num_of_madlibs:
-            print(f'invalid index : must be between 0 and {num_of_madlibs}\n')
+            print(f'{inv_message_prefix}must be between 0 and {num_of_madlibs}\n')
             continue
         
         printMadlib(int(index) - 1)
         return
     
-def validNum(string):
+def isValidNum(string):
     try:
         int(string)
         return True
@@ -92,5 +102,11 @@ def getSuffix(string):
         return suffix
     except ValueError:
         return None
+    
+def isVowel(char):
+    if char in ['a','e','i','o','u',]:
+        return True
+    else:
+        return False
 
 getIndex()
--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,9 @@
-madlibsTuple = (
-    (
-        (
-            'Today, my fabulous camp group went to a\\an ',' amusement park. It was a\\an fun park with lots of cool ',' (plural noun) and enjoyable play structures. When we got there, my kind counselor shouted loudly, "Everybody off the ','." We all pushed out in a\\an terrible hurry. My counselor handed out yellow tickets, and we scurried in. I was so excited! I couldn\'t figure     out    what exciting thing to do first. I saw a\\an scary roller coaster I really liked so, I ',' ran over to get in the long line that had about ',' people in it. When I finally got on the roller coaster I was ','. In fact I was so nervous my two knees were knocking together. This was the ',' ride I had ever been on! In about two minutes I heard the crank and grinding of the gears. That’s when the ride began! When I got to the bottom, I was a\\an little ', ' but I was proud of myself. The rest of the day went ', '. It was a\\an ', ' day at the fun park.'
+templates = (
+    {
+        'paragraph': (
+            'Today, my fabulous camp group went to a',' amusement park. It was a fun park with lots of cool ',' and enjoyable play structures. When we got there, my kind counselor shouted loudly,"Everybody off the ','." We all pushed out in a terrible hurry. My counselor handed out yellow tickets, and we scurried in. I was so excited! I couldn\'t figure out what exciting thing to do first. I saw a scary roller coaster I really liked so, I ',' ran over to get in the long line that had about ',' people in it. When I finally got on the roller coaster I was ','. In fact I was so nervous my two knees were knocking together. This was the ',' ride I had ever been on! In about two minutes I heard the crank and grinding of the gears. That’s when the ride began! When I got to the bottom, I was a little ',' but I was proud of myself. The rest of the day went ','. It was a',' day at the fun park.'
         ),
-        (
+        'fill_in': (
             'adjective',
             'plural noun',
             'noun',
@@ -15,12 +15,12 @@ madlibsTuple = (
             'adverb',
             'adjective'
         )
-    ),
-    (
-        (
-            'Today I went to the zoo. I saw a\\an ',' ',' jumping up and down in its tree. He ',' ',' through the large tunnel that led to its ',' ','. I got some peanuts and passed them through the cage to a\\an gigantic gray ',' towering above my head. Feeding that animal made me hungry. I went to get a\\an ',' scoop of ice cream. It filled my stomach. Afterwards I had to ',' ',' to catch our bus. When I got home I ',' my mom for a\\an ',' day at the zoo.'
+    },
+    {
+        'paragraph': (
+            'Today I went to the zoo. I saw a',' ',' jumping up and down in its tree. He ',' ',' through the large tunnel that led to its ',' ','. I got some peanuts and passed them through the cage to a gigantic gray ',' towering above my head. Feeding that animal made me hungry. I went to get a',' scoop of ice cream. It filled my stomach. Afterwards I had to ',' ',' to catch our bus. When I got home I ',' my mom for a',' day at the zoo.'
         ),
-        (
+        'fill_in': (
             'adjective',
             'Noun',
             'past tense verb',
@@ -34,12 +34,12 @@ madlibsTuple = (
             'past tense verb',
             'adjective'
         )
-    ),
-    (
-        (
-            'When I go to the arcade with my ',' there are lots of games to play. I spend lots of time there with my friends. In the game X-Men you can be different ','. The point of the game is to ',' every robot. You also need to save people. Then you can go to the next level. In the game Star Wars you are Luke Skywalker and you try to destroy every ','. In a\\an car racing/motorcycle racing game you need to beat every computerized vehicle that you are ',' against. There are a\\an whole lot of other cool games. When you play some games you win ',' for certain scores. Once you\'re done you can cash in your tickets to get a\\an big ','. You can save your ',' for another time. When I went to this arcade I didn\'t believe how much fun it would be. So far I have had a\\an lot of fun every time I\'ve been to this great arcade!'
+    },
+    {
+        'paragraph': (
+            'When I go to the arcade with my ',' there are lots of games to play. I spend lots of time there with my friends. In the game X-Men you can be different ','. The point of the game is to ',' every robot. You also need to save people. Then you can go to the next level. In the game Star Wars you are Luke Skywalker and you try to destroy every ','. In a car racing/motorcycle racing game you need to beat every computerized vehicle that you are ',' against. There are a whole lot of other cool games. When you play some games you win ',' for certain scores. Once you\'re done you can cash in your tickets to get a big ','. You can save your ',' for another time. When I went to this arcade I couldn\'t believe how much fun it would be. So far I have had alot of fun every time I\'ve been to this great arcade!'
         ),
-        (
+        'fill_in': (
             'plural noun',
             'plural noun',
             'verb',
@@ -49,5 +49,5 @@ madlibsTuple = (
             'noun',
             'plural noun'
         )
-    )
+    },
 )
